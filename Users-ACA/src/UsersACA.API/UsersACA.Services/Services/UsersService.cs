449b345dc9abb713661d792e1a4dc1cb6b28ae3b
christian-cell <cristohp74@gmail.com>
2025-03-05 17:53:44 +0100

    moving dockerfile to

---
+++ Users-ACA/src/UsersACA.API/UsersACA.Services/Services/UsersService.cs
@@ -0,0 +1,14 @@
+using UsersACA.Services.Abstractions;
+
+namespace UsersACA.Services.Services
+{
+    public class UsersService: IUsersService
+    {
+        public string GreatPeople()
+        {
+            /*nuevo mensaje*/
+            return "prueba sin usar continuos deployment";
+        }
+    }
+};
+
